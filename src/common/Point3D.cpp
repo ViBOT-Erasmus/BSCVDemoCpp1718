30c78783f224b9d9f6b7c763cf304a536c1c621c
Ebenezer Kwaku Antwi <you@example.com>
2018-03-20 10:26:47 +0100

    My 3D point class and test

---
+++ src/common/Point3D.cpp
@@ -0,0 +1,9 @@
+#include "Point3D.hpp"
+
+tdpoint::tdpoint (double xa, double ya, double za)
+{
+	x = xa;
+	y = ya;
+	z = za;
+}
+
---
+++ src/common/Point3D.hpp
@@ -0,0 +1,12 @@
+#ifndef POINT3D_H
+#define POINT3D_H
+
+class tdpoint {
+		public:
+		double x;
+		double y;
+		double z;
+		tdpoint(double, double,double);
+};
+
+#endif
